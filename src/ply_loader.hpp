4c47098c867707edea86c6edfcd95183e8ac83b2
Tomas Kremel <kremelt@gmail.com>
2022-11-01 23:19:57 +0100

    Normals generated by pymeshlab package are doubles, adding support for that.

--- src/ply_loader.hpp
+++ src/ply_loader.hpp
@@ -26,7 +26,22 @@ void load_ply(
     input_normals_x = ply.getElement("vertex").getProperty<float>("nx");
     input_normals_y = ply.getElement("vertex").getProperty<float>("ny");
     input_normals_z = ply.getElement("vertex").getProperty<float>("nz");
-  } catch (const std::runtime_error &) {}
+  } catch (const std::runtime_error &) {
+    try {
+      std::vector<double> input_normals_x_, input_normals_y_, input_normals_z_;
+      input_normals_x_ = ply.getElement("vertex").getProperty<double>("nx");
+      input_normals_x.resize(input_normals_x_.size());
+      std::transform(input_normals_x_.begin(), input_normals_x_.end(), input_normals_x.begin(), [](auto&& i){ return (float)i; });
+      input_normals_y_ = ply.getElement("vertex").getProperty<double>("ny");
+      input_normals_y.resize(input_normals_y_.size());
+      std::transform(input_normals_y_.begin(), input_normals_y_.end(), input_normals_y.begin(), [](auto&& i){ return (float)i; });
+      input_normals_z_ = ply.getElement("vertex").getProperty<double>("nz");
+      input_normals_z.resize(input_normals_z_.size());
+      std::transform(input_normals_z_.begin(), input_normals_z_.end(), input_normals_z.begin(), [](auto&& i){ return (float)i; });
+    } catch (const std::runtime_error &) {
+      throw std::runtime_error("For splatting, normals are necessary!");
+    }
+  }
   std::vector<std::vector<unsigned long>> input_faces;
   try {
     input_faces = ply.getFaceIndices();
